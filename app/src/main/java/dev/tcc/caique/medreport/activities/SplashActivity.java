8c8b0b4d16f8ccf52b38138a910a669bc715d20d
jcaiqueoliveira <caique.oliveira@hub7.com.br>
2016-01-29 21:32:37 -0300

    add splash

---
+++ app/src/main/java/dev/tcc/caique/medreport/activities/SplashActivity.java
@@ -0,0 +1,19 @@
+package dev.tcc.caique.medreport.activities;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import dev.tcc.caique.medreport.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
