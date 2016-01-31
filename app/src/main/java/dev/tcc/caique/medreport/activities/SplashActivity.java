d6188a1d10db38f9019e89841b5df723126bcea8
jcaiqueoliveira <caique.oliveira@hub7.com.br>
2016-01-30 21:13:54 -0300

    add code StatusConn class, and update splash activity

--- app/src/main/java/dev/tcc/caique/medreport/activities/SplashActivity.java
+++ app/src/main/java/dev/tcc/caique/medreport/activities/SplashActivity.java
@@ -1,13 +1,13 @@
 package dev.tcc.caique.medreport.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.os.Handler;
 
 import dev.tcc.caique.medreport.R;
+import dev.tcc.caique.medreport.utils.StatusConn;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -15,5 +15,16 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-    }
+        if(!StatusConn.isOnline(getApplicationContext())){
+            Snackbar.make(findViewById(R.id.coordinatorLayout),"Sem conexão com a internet",Snackbar.LENGTH_LONG).show();
+        }
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+
+                    startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                    finish();
+                }
+            }, 2000);
+        }
 }
--- app/src/main/java/dev/tcc/caique/medreport/utils/StatusConn.java
+++ app/src/main/java/dev/tcc/caique/medreport/utils/StatusConn.java
@@ -3,5 +3,19 @@ package dev.tcc.caique.medreport.utils;
 /**
  * Created by caique on 29/01/16.
  */
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 public class StatusConn {
+    public static boolean isOnline(Context context) {
+        boolean retorno = false;
+        ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo netInfo = cm.getActiveNetworkInfo();
+        if (netInfo != null && netInfo.isConnectedOrConnecting()) {
+            retorno = true;
+        }
+        return retorno;
+    }
+
 }
