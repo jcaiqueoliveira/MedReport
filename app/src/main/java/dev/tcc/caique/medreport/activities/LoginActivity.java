ebb04ceec7f4901d058c1f4afd0795fea525a51d
jcaiqueoliveira <caique.oliveira@hub7.com.br>
2016-01-30 22:13:37 -0300

    add loginActivity

---
+++ app/src/main/java/dev/tcc/caique/medreport/activities/LoginActivity.java
@@ -0,0 +1,15 @@
+package dev.tcc.caique.medreport.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import dev.tcc.caique.medreport.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
--- app/src/main/java/dev/tcc/caique/medreport/models/Profile.java
+++ app/src/main/java/dev/tcc/caique/medreport/models/Profile.java
@@ -4,4 +4,8 @@ package dev.tcc.caique.medreport.models;
  * Created by caique on 29/01/16.
  */
 public class Profile {
+    private String nome;
+    private String email;
+    private String idade;
+    private int tipo;
 }
