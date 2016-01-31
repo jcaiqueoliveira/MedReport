a6b0e44aa1ab11ea7ea11dd560bef3c370eccebc
jcaiqueoliveira <caique.oliveira@hub7.com.br>
2016-01-31 12:15:34 -0300

    add classe model Report

---
+++ app/src/main/java/dev/tcc/caique/medreport/models/Report.java
@@ -0,0 +1,14 @@
+package dev.tcc.caique.medreport.models;
+
+import android.graphics.Bitmap;
+
+import java.util.ArrayList;
+
+/**
+ * Created by caique on 31/01/16.
+ */
+public class Report {
+    private String title;
+    private String description;
+    private ArrayList<Bitmap> images;
+}
