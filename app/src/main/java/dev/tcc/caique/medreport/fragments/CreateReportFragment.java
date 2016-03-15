96751d8d14eab168647a629440f49bc22816768e
Caique <Caique>
2016-03-15 02:08:10 -0300

    send report to firebase

--- app/src/main/java/dev/tcc/caique/medreport/fragments/CreateReportFragment.java
+++ app/src/main/java/dev/tcc/caique/medreport/fragments/CreateReportFragment.java
@@ -2,30 +2,34 @@ package dev.tcc.caique.medreport.fragments;
 
 
 import android.app.Activity;
-import android.content.ClipData;
 import android.content.Intent;
-import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.v4.app.Fragment;
+import android.util.Base64;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.GridView;
 
+import com.firebase.client.Firebase;
 import com.gun0912.tedpicker.ImagePickerActivity;
 
-import java.io.File;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -33,6 +37,7 @@ import butterknife.OnClick;
 import dev.tcc.caique.medreport.R;
 import dev.tcc.caique.medreport.activities.MainActivity;
 import dev.tcc.caique.medreport.adapters.ImageAdapter;
+import dev.tcc.caique.medreport.models.Report;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -42,6 +47,10 @@ public class CreateReportFragment extends Fragment {
     GridView gridView;
     @Bind(R.id.addImages)
     Button btnAdd;
+    @Bind(R.id.title)
+    EditText title;
+    @Bind(R.id.description)
+    EditText description;
 
     private static final int INTENT_REQUEST_GET_IMAGES = 13;
 
@@ -60,44 +69,102 @@ public class CreateReportFragment extends Fragment {
         v = inflater.inflate(R.layout.fragment_create_report, container, false);
         ButterKnife.bind(this, v);
         ((MainActivity) getActivity()).fab.hide();
+        setHasOptionsMenu(true);
         return v;
     }
 
     @OnClick(R.id.addImages)
-    public void addImages(){
+    public void addImages() {
         //Prompt dialog to user select between camera and gallery
         openGalleryToSelectImages();
     }
 
-    private void openGalleryToSelectImages(){
-        Intent intent  = new Intent(getActivity(), ImagePickerActivity.class);
+    private void openGalleryToSelectImages() {
+        Intent intent = new Intent(getActivity(), ImagePickerActivity.class);
         startActivityForResult(intent, INTENT_REQUEST_GET_IMAGES);
     }
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == INTENT_REQUEST_GET_IMAGES && resultCode == Activity.RESULT_OK ) {
-            ArrayList<Uri>  imageUris = data.getParcelableArrayListExtra(ImagePickerActivity.EXTRA_IMAGE_URIS);
-            ArrayList<Bitmap> images = getBitmapsFromImageUris(imageUris);
-            if (imageUris.size()>0){
-                gridView.setAdapter(new ImageAdapter(getActivity(),images));
+        if (requestCode == INTENT_REQUEST_GET_IMAGES && resultCode == Activity.RESULT_OK) {
+            ArrayList<Uri> imageUris = data.getParcelableArrayListExtra(ImagePickerActivity.EXTRA_IMAGE_URIS);
+            images = getBitmapsFromImageUris(imageUris);
+            if (imageUris.size() > 0) {
+                gridView.setAdapter(new ImageAdapter(getActivity(), images));
             }
         }
     }
 
-    private ArrayList<Bitmap> getBitmapsFromImageUris(ArrayList<Uri> uris){
+    private ArrayList<Bitmap> getBitmapsFromImageUris(ArrayList<Uri> uris) {
         ArrayList<Bitmap> bitmaps = new ArrayList<>();
-        for(Uri uri : uris){
+        for (Uri uri : uris) {
             try {
-                InputStream image_stream = getActivity().getContentResolver().openInputStream(Uri.parse("file://"+uri));
-                Bitmap bitmap= BitmapFactory.decodeStream(image_stream );
+                InputStream image_stream = getActivity().getContentResolver().openInputStream(Uri.parse("file://" + uri));
+                Bitmap bitmap = BitmapFactory.decodeStream(image_stream);
                 bitmaps.add(bitmap);
-                bitmaps.add(MediaStore.Images.Media.getBitmap(getActivity().getContentResolver(),uri));
+                bitmaps.add(MediaStore.Images.Media.getBitmap(getActivity().getContentResolver(), uri));
             } catch (IOException e) {
                 e.printStackTrace();
             }
         }
         return bitmaps;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.save_menu, menu);
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.save:
+                try {
+                    Firebase ref = new Firebase("https://medreportapp.firebaseio.com/");
+                    // Map<String, Map<String, Report>> report = new HashMap<String,Firebase>();
+                    Report r = new Report(title.getText().toString(), description.getText().toString());//, createListImagesCompress());
+                    Map<String, String> post2 = new HashMap<String, String>();
+                    post2.put("title", "alanisawesome");
+                    post2.put("description", "The Turing Machine");
+                    ref.child("users").child("52a1cba1-a170-49ca-891d-65ae3a38d84f").child("report").push().setValue(post2);
+                    String uid = ref.getKey();
+                    ArrayList<String> strings = createListImagesCompress();
+                    for (String s : strings) {
+                        Log.i("aqui", "uid" + uid);
+                        Map<String, String> images = new HashMap<String, String>();
+                        images.put("image", s);
+                        ref.child("users").child("52a1cba1-a170-49ca-891d-65ae3a38d84f").child("report").
+                                child(uid)
+                                .child("image")
+                                .push().setValue(images);
+                    }
+
+                    //report.put("report", ref.push());
+                    // usersRef.setValue(users);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private ArrayList<String> createListImagesCompress() {
+        ArrayList<String> listImages = new ArrayList<>();
+        if (images.size() > 0) {
+            for (Bitmap bmp : images) {
+                ByteArrayOutputStream bYtE = new ByteArrayOutputStream();
+                bmp.compress(Bitmap.CompressFormat.PNG, 100, bYtE);
+                bmp.recycle();
+                byte[] byteArray = bYtE.toByteArray();
+                String imageFile = Base64.encodeToString(byteArray, Base64.DEFAULT);
+                listImages.add(imageFile);
+            }
+        } else {
+            Log.i("maior que ", "0");
+        }
+        return listImages;
     }
 }
--- app/src/main/java/dev/tcc/caique/medreport/models/Report.java
+++ app/src/main/java/dev/tcc/caique/medreport/models/Report.java
@@ -2,13 +2,20 @@ package dev.tcc.caique.medreport.models;
 
 import android.graphics.Bitmap;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 
 /**
  * Created by caique on 31/01/16.
  */
-public class Report {
+public class Report implements Serializable{
     private String title;
     private String description;
-    private ArrayList<Bitmap> images;
+    private ArrayList<String> images;
+
+    public Report(String title, String description){//, ArrayList<String> images){
+        this.title = title;
+        this.description = description;
+        this.images = images;
+    }
 }
