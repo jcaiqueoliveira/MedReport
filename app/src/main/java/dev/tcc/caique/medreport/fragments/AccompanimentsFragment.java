26f0c07866d00e51e9b8a9dbf1c029af7bcde199
Caique <Caique>
2016-04-30 12:08:59 -0300

    add function to show profile in accompanimentsfragment

--- app/src/main/java/dev/tcc/caique/medreport/fragments/AccompanimentsFragment.java
+++ app/src/main/java/dev/tcc/caique/medreport/fragments/AccompanimentsFragment.java
@@ -1,15 +1,21 @@
 package dev.tcc.caique.medreport.fragments;
 
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.widget.AppCompatEditText;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
@@ -67,12 +73,31 @@ public class AccompanimentsFragment extends Fragment {
 
                         @Override
                         public void onDataChange(DataSnapshot dataSnapshot) {
-                            for (DataSnapshot ds : dataSnapshot.getChildren()) {
+                            for (final DataSnapshot ds : dataSnapshot.getChildren()) {
+                                Log.e("dados", ds.toString());
                                 viewHolderAccompaniments.namePerson.setText((String) ds.child("name").getValue());
                                 url[0] = (String) ds.child("profile").child("profileUrl").getValue();
                                 if (url[0] != null) {
-                                    Glide.with(getActivity()).load(url[0]).placeholder(android.R.drawable.progress_indeterminate_horizontal).into(viewHolderAccompaniments.thumbnail);
+                                    Glide.with(getActivity()).load(url[0]).into(viewHolderAccompaniments.thumbnail);
                                 }
+                                viewHolderAccompaniments.informations.setOnClickListener(new View.OnClickListener() {
+                                    @Override
+                                    public void onClick(View v) {
+                                        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+                                        if (ds.child("type").getValue().toString().equals(Constants.TYPE_MEDICAL)) {
+                                            builder.setView(getProfileMedicalInfo(ds, url[0]));
+                                        } else {
+                                            builder.setView(getProfilePacientInfo(ds, url[0]));
+                                        }
+                                        builder.setPositiveButton("Fechar", new DialogInterface.OnClickListener() {
+                                            @Override
+                                            public void onClick(DialogInterface dialog, int which) {
+                                                dialog.dismiss();
+                                            }
+                                        });
+                                        builder.create().show();
+                                    }
+                                });
                             }
                         }
 
@@ -94,6 +119,12 @@ public class AccompanimentsFragment extends Fragment {
                             startActivity(i);
                         }
                     });
+                    viewHolderAccompaniments.informations.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+
+                        }
+                    });
                 }
             };
             accompanimentsList.setAdapter(adapter);
@@ -113,6 +144,7 @@ public class AccompanimentsFragment extends Fragment {
     public static class ViewHolderAccompaniments extends RecyclerView.ViewHolder {
         public CircleImageView thumbnail;
         public TextView namePerson;
+        public ImageView informations;
         public View view;
 
         public ViewHolderAccompaniments(View v) {
@@ -120,6 +152,7 @@ public class AccompanimentsFragment extends Fragment {
             this.view = v;
             thumbnail = (CircleImageView) v.findViewById(R.id.thumbnail);
             namePerson = (TextView) v.findViewById(R.id.nameAccompanimentList);
+            informations = (ImageView) v.findViewById(R.id.informations);
         }
     }
 
@@ -130,4 +163,90 @@ public class AccompanimentsFragment extends Fragment {
             noItem.setVisibility(View.GONE);
         }
     }
-}
+
+    private View getProfileMedicalInfo(DataSnapshot ds, String url) {
+        final View view = getActivity().getLayoutInflater().inflate(R.layout.fragment_profile_medical, null);
+        CircleImageView imageProfile = (CircleImageView) view.findViewById(R.id.imgProfile);
+        EditText specialization = (EditText) view.findViewById(R.id.specialization);
+        EditText crm = (EditText) view.findViewById(R.id.crm);
+        AppCompatEditText name = (AppCompatEditText) view.findViewById(R.id.nameMedical);
+        name.setText(ds.child("name").getValue().toString());
+        name.setEnabled(false);
+        if (!TextUtils.isEmpty(url)) {
+            Glide.with(getActivity()).load(url).into(imageProfile);
+        }
+        if (!TextUtils.isEmpty((String) ds.child("profile").child("specialization").getValue()))
+            specialization.setText((String) ds.child("profile").child("specialization").getValue());
+        else
+            specialization.setVisibility(View.GONE);
+
+        if (!TextUtils.isEmpty((String) ds.child("profile").child("crm").getValue()))
+            crm.setText((String) ds.child("profile").child("crm").getValue());
+        else
+            crm.setVisibility(View.GONE);
+
+        specialization.setEnabled(false);
+        crm.setEnabled(false);
+        return view;
+    }
+
+    private View getProfilePacientInfo(DataSnapshot ds, String url) {
+        final View view = getActivity().getLayoutInflater().inflate(R.layout.fragment_profile_pacient, null);
+        CircleImageView imgProfile = (CircleImageView) view.findViewById(R.id.imgProfile);
+        AppCompatEditText edtName = (AppCompatEditText) view.findViewById(R.id.edtName);
+        AppCompatEditText edtAge = (AppCompatEditText) view.findViewById(R.id.edtAge);
+        AppCompatEditText edtGender = (AppCompatEditText) view.findViewById(R.id.edtGender);
+        AppCompatEditText edtHeight = (AppCompatEditText) view.findViewById(R.id.edtHeight);
+        AppCompatEditText weight = (AppCompatEditText) view.findViewById(R.id.edtWeight);
+        AppCompatEditText edtProblem1 = (AppCompatEditText) view.findViewById(R.id.edtProblem1);
+        AppCompatEditText edtProblem2 = (AppCompatEditText) view.findViewById(R.id.edtProblem2);
+        AppCompatEditText edtProblem3 = (AppCompatEditText) view.findViewById(R.id.edtProblem3);
+        if (!TextUtils.isEmpty(url))
+            Glide.with(getActivity()).load(url).into(imgProfile);
+        edtName.setText(ds.child("name").getValue().toString());
+        if (!TextUtils.isEmpty(ds.child("profile").child("age").getValue().toString()))
+            edtAge.setText(ds.child("profile").child("age").getValue().toString());
+            else
+            edtAge.setVisibility(View.GONE);
+
+        if (!TextUtils.isEmpty(ds.child("profile").child("gender").getValue().toString()))
+            edtGender.setText(ds.child("profile").child("gender").getValue().toString());
+        else
+            edtGender.setVisibility(View.GONE);
+
+        if (!TextUtils.isEmpty(ds.child("profile").child("stature").getValue().toString()))
+            edtHeight.setText(ds.child("profile").child("stature").getValue().toString());
+        else
+            edtHeight.setVisibility(View.GONE);
+
+        if (!TextUtils.isEmpty(ds.child("profile").child("weight").getValue().toString()))
+            weight.setText(ds.child("profile").child("weight").getValue().toString());
+        else
+            weight.setVisibility(View.GONE);
+
+        if (!TextUtils.isEmpty(ds.child("profile").child("healthProblem").getValue().toString()))
+            edtProblem1.setText(ds.child("profile").child("healthProblem").getValue().toString());
+        else
+            edtProblem1.setVisibility(View.GONE);
+
+        if (!TextUtils.isEmpty(ds.child("profile").child("allergy").getValue().toString()))
+            edtProblem2.setText(ds.child("profile").child("allergy").getValue().toString());
+        else
+            edtProblem2.setVisibility(View.GONE);
+
+        if (!TextUtils.isEmpty(ds.child("profile").child("drugAllergy").getValue().toString()))
+            edtProblem3.setText(ds.child("profile").child("drugAllergy").getValue().toString());
+        else
+            edtProblem3.setVisibility(View.GONE);
+
+        edtName.setEnabled(false);
+        edtAge.setEnabled(false);
+        edtGender.setEnabled(false);
+        edtHeight.setEnabled(false);
+        weight.setEnabled(false);
+        edtProblem1.setEnabled(false);
+        edtProblem2.setEnabled(false);
+        edtProblem3.setEnabled(false);
+        return view;
+        }
+    }
